5331aeb1a63c451be83a9e5cb3b10cd311f3bbc7
Kieran <kieransweeney52@gmail.com>
2023-01-19 16:37:38 +0000

    script for new users

--- retry.ps1
+++
@@ -1,4 +0,0 @@
-$PasswordProfile = @{
-    Password = 'AB12CD34!?'
-    }
-  New-MgUser -DisplayName 'Kieran Sweeney' -PasswordProfile $PasswordProfile -AccountEnabled -MailNickName 'Kieran' -UserPrincipalName 'kierans@blanknessplc.onmicrosoft.com'  -UsageLocation GB
